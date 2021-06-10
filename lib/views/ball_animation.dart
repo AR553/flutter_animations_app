6bb943b8433b0ce644af7031a8917d841c5a0910
abeer.rizvi <abeerhussain99@gmail.com>
2021-06-10 10:58:21 +0500

    add bouncing ball animation-fix

--- lib/views/ball_animation.dart
+++ lib/views/ball_animation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class BallAnimationPage extends StatefulWidget {
   const BallAnimationPage({Key? key}) : super(key: key);
@@ -14,7 +15,7 @@ class _BallAnimationPageState extends State<BallAnimationPage> with SingleTicker
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this, duration: Duration(seconds: 1))..forward();
+    _controller = AnimationController(vsync: this, duration: Duration(milliseconds: 800),upperBound: .9)..forward()..repeat(reverse: true);
   }
 
   @override
@@ -24,7 +25,7 @@ class _BallAnimationPageState extends State<BallAnimationPage> with SingleTicker
   }
 
   double curve(double value){
-    return Curves.ease.transform(value);
+    return Curves.easeOutExpo.transform(value);
   }
 
   @override
@@ -32,42 +33,47 @@ class _BallAnimationPageState extends State<BallAnimationPage> with SingleTicker
     return Scaffold(
         appBar: AppBar(
           title: Text('Bouncy'),
+          centerTitle: true,
         ),
         body: AnimatedBuilder(
           animation: _controller,
           builder: (BuildContext context, Widget? child) => Center(
             child: Container(
-              child: Stack(
-                alignment: Alignment.bottomCenter,
-                children: [
-                  Container(
-                    width: 160 - curve(_controller.value)*10,
-                    height: 140 + curve(_controller.value)*10,
-                    transform: Transform.translate(offset: Offset(0, -curve(_controller.value)*300)).transform,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(100),
-                      color: Colors.redAccent,
-                    ),
-                    child: ClipOval(
-                      child: RotatedBox(
-                        quarterTurns: 1,
-                        child: Image.network('https://static.vecteezy.com/system/resources/thumbnails/000/135/839/small/basketball-texture-free-vector.png',fit: BoxFit.cover,
+              padding: EdgeInsets.only(top: 100),
+              child: Transform.scale(
+                scale: .5,
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: [
+                    Container(
+                      width: 160 - curve(_controller.value)*10,
+                      height: 140 + curve(_controller.value)*10,
+                      transform: Transform.translate(offset: Offset(0, -curve(_controller.value)*300)).transform,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        color: Colors.redAccent,
+                      ),
+                      child: ClipOval(
+                        child: RotatedBox(
+                          quarterTurns: 1,
+                          child: Image.network('https://static.vecteezy.com/system/resources/thumbnails/000/135/839/small/basketball-texture-free-vector.png',fit: BoxFit.cover,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [BoxShadow(color: Colors.black38.withOpacity(curve(1-(_controller.value))),blurRadius: 10,offset: Offset(0,30))],
-                      borderRadius: BorderRadius.all(Radius.elliptical(curve(_controller.value)*100+100, curve(_controller.value)*50+40)),
+                    Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [BoxShadow(color: Colors.black38.withOpacity(curve(1-(_controller.value))),blurRadius: 10,offset: Offset(0,30))],
+                        borderRadius: BorderRadius.all(Radius.elliptical(curve(_controller.value)*100+100, curve(_controller.value)*50+40)),
 
+                      ),
+                      width: 80 - curve(_controller.value)*80 + 30,
+                      height: 40 - curve(_controller.value)*40 + 5,
+                      margin: EdgeInsets.symmetric(horizontal: 100),
+                      child: Container(),
                     ),
-                    width: 80 - curve(_controller.value)*80 + 30,
-                    height: 40 - curve(_controller.value)*40 + 5,
-                    margin: EdgeInsets.symmetric(horizontal: 100),
-                    child: Container(),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
